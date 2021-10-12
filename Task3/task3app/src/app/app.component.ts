e03176650b6b3e512e2854a0cb004d9ec1a20a01
MkBrt <mysspam@ukr.net>
2021-10-13 01:29:20 +0300

    Task 3 is completed.

---
+++ Task3/task3app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  textareaContents: string = "";
+  sumPrices(): number {
+    let purchases: string[] = this.textareaContents.split("\n");
+    let prices: Array<number> = [];
+    for (let product of purchases) {
+      let emptyOrNumericStrings: string[] = product.split(/[^\d.-]+/g);
+      for (let emptyOrNumericString of emptyOrNumericStrings) {
+        if (emptyOrNumericString) {
+          let price: number = parseFloat(emptyOrNumericString);
+          if (price) {
+            prices.push(price);
+          }
+          break;
+        }
+      }
+    }
+    let sum: number = 0;
+    for (let price of prices) {
+      sum += price;
+    }
+    return sum;
+  }
+}
